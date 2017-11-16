aded3f58386047962f44197bf44743e94524096c
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-15 17:38:11 -0800

    Fix error when creating a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
     def new
         @topic = Topic.find(params[:topic_id])
         @post = Post.new
+        @post.topic = @topic
     end
 
     def edit
