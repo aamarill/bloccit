b139914f93572e5ba41c1bb41e9236d181d80871
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-17 11:43:12 -0800

    Complete checkpoint 24

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	before_save { self.email = email.downcase if email.present? }
+	before_save { self.email = email.downcase  if email.present? }
 
 	 # #3
 	   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
