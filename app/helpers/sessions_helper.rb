b3154db9412626417154c7dd65ecae07ed28f780
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-17 16:18:47 -0800

    Complete assignment 25

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,19 @@
 module SessionsHelper
 	def create_session(user)
-	     session[:user_id] = user.id
-	   end
+		session[:user_id] = user.id
+	end
 
-	   def destroy_session(user)
-	     session[:user_id] = nil
-	   end
+	def destroy_session(user)
+		session[:user_id] = nil
+	end
 
-	   def current_user
-	     User.find_by(id: session[:user_id])
-	   end
+	def current_user
+		User.find_by(id: session[:user_id])
+	end
 
+	def avatar_url(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		"https://s.gravatar.com/avatar/#{gravatar_id}?s=80"
+		#https://s.gravatar.com/avatar/814a973972c153bdaae27f3a9ea4926e?s=80
+	end
 end
