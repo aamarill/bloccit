ce0b84e2c24b044cec8dd8ac3e973593128a38ec
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-21 13:23:43 -0800

    Complete checkpoint 27

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+
+	# Stuff that's in here will be available to the corresponding view (Post view in this case)
+
+	def user_is_authorized_for_post?(post)
+		current_user && (current_user == post.user || current_user.admin?)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,18 @@ class User < ApplicationRecord
 	has_many :posts, dependent: :destroy
 
 	before_save { self.email = email.downcase  if email.present? }
+	before_save { self.role ||= :member}
 
-	 # #3
-	   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-	 # #4
-	  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-	  validates :password, length: { minimum: 6 }, allow_blank: true
-	 # #5
-	   validates :email,
-	             presence: true,
-	             uniqueness: { case_sensitive: false },
-	             length: { minimum: 3, maximum: 254 }
+	validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
-	 # #6
-	   has_secure_password
+	validates :email,
+	  		  presence: true,
+			  uniqueness: { case_sensitive: false },
+			  length: { minimum: 3, maximum: 254 }
+	validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+	validates :password, length: { minimum: 6 }, allow_blank: true
+
+	has_secure_password
+
+	enum role: [:member, :admin]
 end
