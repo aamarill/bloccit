6c818e2c4c86ffd5ca689cf4309c44e3f28cf98c
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-20 21:14:07 -0800

    Complete checkpoint 26

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+	has_many :posts, dependent: :destroy
+
 	before_save { self.email = email.downcase  if email.present? }
 
 	 # #3
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'random_data'
+
+# Create Users
+5.times do
+	User.create!(
+	name: RandomData.random_name,
+	email: RandomData.random_email,
+	password: RandomData.random_sentence
+	)
+end
+users = User.all
 
 # Create topics
 15.times do
@@ -21,6 +23,7 @@ topics = Topic.all
 # Create posts
 50.times do
 		Post.create!(
+			user: users.sample,
 			topic: topics.sample,
 			title: RandomData.random_sentence,
 			body:  RandomData.random_paragraph
@@ -37,7 +40,15 @@ posts = Post.all
 	)
 end
 
+user = User.first
+user.update_attributes!(
+	email: 'aamarill.engr@gmail.com',
+	password: 'helloworld'
+)
+
+
 puts "Seed Finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
