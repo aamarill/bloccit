64ca3c510e5514c095f798f9e9c5c826da49083b
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-21 19:39:28 -0800

    Complete assignment 27

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,6 @@ module PostsHelper
 	# Stuff that's in here will be available to the corresponding view (Post view in this case)
 
 	def user_is_authorized_for_post?(post)
-		current_user && (current_user == post.user || current_user.admin?)
+		current_user && (current_user == post.user || current_user.admin? || current_user.moderator?)
 	end
 end
